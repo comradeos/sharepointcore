ddd8ec726f919bbd7d1ccec14bf466a4bebb9202
Iaroslav Os <osmnbx@gmail.com>
2026-09-18 10:07:37 +0300

    new project

---
+++ SPFXTask_Oseledko_YY/src/webparts/spfxTaskOseledkoYy/components/ISpfxTaskOseledkoYyProps.ts
@@ -0,0 +1,6 @@
+export interface ISpfxTaskOseledkoYyProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  userDisplayName: string;
+}
---
+++ SPFXTask_Oseledko_YY/src/webparts/spfxTaskOseledkoYy/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxTaskOseledkoYyWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxTaskOseledkoYyWebPartStrings' {
+  const strings: ISpfxTaskOseledkoYyWebPartStrings;
+  export = strings;
+}
